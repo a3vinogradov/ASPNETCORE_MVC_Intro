d8f255ddd958c24915417ec5392a06efa9fa8651
Vinogradov Alexander <a3vinogradov@gmail.com>
2022-11-08 17:47:19 +0300

    Job-021-2 Metanit. Введение в ASP.NET Core MVC. Глава 2

---
+++ Ch02-05_DependancyInjection/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ch02_05_DependancyInjection
+
+{
+    public class HomeController : Controller
+    {
+        readonly ITimeService _timeService;
+
+        public HomeController(ITimeService timeService) 
+        { 
+            _timeService = timeService;
+        }
+
+        public IActionResult Index()
+        {
+            string content = "Hello world!!! <br>";
+            content += $"{_timeService.Time.ToString()}<br>";
+            content += "<a href = \"Home/Page1\"> attribute FromServises </a><br>";
+            content += "<a href = \"Home/Page2\"> HttpContext.RequestServices.GetService </a><br>";
+
+            return new HTMLResult(content);
+        }
+
+        public string Page1([FromServices] ITimeService timeService)
+        {
+            return timeService.Time.ToString();
+        }
+        public string Page2()
+        {
+            ITimeService? timeService = HttpContext.RequestServices.GetService<ITimeService>();
+            return timeService?.Time ?? "Undefined";
+        }
+    }
+}
---
+++ Ch02-05_DependancyInjection/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<ITimeService, SimpleTimeService>();
+var app = builder.Build();
+
+// устанавливаем сопоставление маршрутов с контроллерами
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+public interface ITimeService
+{
+    string Time { get; }
+}
+public class SimpleTimeService : ITimeService
+{
+    public string Time => DateTime.Now.ToString("hh:mm:ss");
+}
